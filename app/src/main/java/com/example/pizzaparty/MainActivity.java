d7b4f74518e03a284e31fd70dcd1e6e0573649eb
kag6020 <80540982+kag6020@users.noreply.github.com>
2021-08-29 16:42:32 -0400

    updated radio buttons

--- app/src/main/java/com/example/pizzaparty/MainActivity.java
+++ app/src/main/java/com/example/pizzaparty/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText mNumAttendEditText;
     private TextView mNumPizzasTextView;
+    private RadioGroup mHowHungryRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +22,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mNumAttendEditText = findViewById(R.id.num_attend_edit_text);
         mNumPizzasTextView = findViewById(R.id.num_pizzas_text_view);
+        mHowHungryRadioGroup = findViewById(R.id.hungry_radio_group);
     }
 
     public void calculateClick(View view) {
         String numAttendStr = mNumAttendEditText.getText().toString();
         int numAttend = Integer.parseInt(numAttendStr);
 
-        int slicesPerPerson = 4;
+        // Determine how many slices on average each person will eat
+        int slicesPerPerson = 0;
+        int checkedId = mHowHungryRadioGroup.getCheckedRadioButtonId();
+        if (checkedId == R.id.light_radio_button) {
+            slicesPerPerson = 2;
+        }
+        else if (checkedId == R.id.medium_radio_button) {
+            slicesPerPerson = 3;
+        }
+        else if (checkedId == R.id.ravenous_radio_button) {
+            slicesPerPerson = 4;
+        }
+
+
         int totalPizzas = (int) Math.ceil(numAttend * slicesPerPerson /
                 (double) SLICES_PER_PIZZA);
         mNumPizzasTextView.setText("Total pizzas: " + totalPizzas);
