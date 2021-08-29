1541cd13f5c1a79e4ad407c80a18aaa073dccc22
kag6020 <80540982+kag6020@users.noreply.github.com>
2021-08-29 15:59:18 -0400

    pizza

---
+++ app/src/main/java/com/example/pizzaparty/MainActivity.java
@@ -0,0 +1,35 @@
+
+package com.example.pizzaparty;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final int SLICES_PER_PIZZA = 8;
+
+    private EditText mNumAttendEditText;
+    private TextView mNumPizzasTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mNumAttendEditText = findViewById(R.id.num_attend_edit_text);
+        mNumPizzasTextView = findViewById(R.id.num_pizzas_text_view);
+    }
+
+    public void calculateClick(View view) {
+        String numAttendStr = mNumAttendEditText.getText().toString();
+        int numAttend = Integer.parseInt(numAttendStr);
+
+        int slicesPerPerson = 4;
+        int totalPizzas = (int) Math.ceil(numAttend * slicesPerPerson /
+                (double) SLICES_PER_PIZZA);
+        mNumPizzasTextView.setText("Total pizzas: " + totalPizzas);
+    }
+}
